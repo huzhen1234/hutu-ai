8a63b0e918e5c2bb537b6a97064e2bb51566a4d5
huzhen <2027280373@qq.com>
2025-06-10 00:09:22 +0800

    ai架子创建

---
+++ src/main/java/org/hutu/com/HutuAiApplication.java
@@ -0,0 +1,13 @@
+package org.hutu.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HutuAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HutuAiApplication.class, args);
+    }
+
+}
